aff3bc09ffb459f005bc6c5900db014828a062ec
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-07-21 16:16:44 +0200

    Adding Unit type

---
+++ src/CSharpx/Unit.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CSharpx
+{
+    public struct Unit : IEquatable<Unit>
+    {
+        private static readonly Unit @default = new Unit();
+
+        public bool Equals(Unit other)
+        {
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Unit;
+        }
+
+        public override int GetHashCode()
+        {
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return "()";
+        }
+
+        public static bool operator ==(Unit first, Unit second)
+        {
+            return true;
+        }
+
+        public static bool operator !=(Unit first, Unit second)
+        {
+            return false;
+        }
+
+        public static Unit Default { get { return @default; } }
+    }
+}
