bd1018e303ae0d97858027568b4f96efde91e02a
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-07-21 06:06:14 +0200

    Adding EnumerableExtensions.cs to project

---
+++ src/CSharpx/EnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿#define CSHARPX_PUBLIC // Comment this to set visibility to internal.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpx
+{
+#if CSHARPX_PUBLIC
+    public
+#endif
+    partial class EnumerableExtensions
+    {
+
+    }
+}
