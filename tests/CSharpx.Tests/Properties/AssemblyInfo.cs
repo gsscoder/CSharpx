27dc4312309fae3ba85c4f703797c5bf5d9d565a
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-08-28 19:05:08 +0200

    Added tests project stub

---
+++ tests/CSharpx.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CSharpx.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CSharpx.Tests")]
+[assembly: AssemblyCopyright("Copyright (c) 2015 Giacomo Stelluti Scala")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0-pre")]
