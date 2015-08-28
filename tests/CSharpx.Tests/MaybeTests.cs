ba2ebb189fd966d5bf2ce19240b76efb6520e621
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-08-28 20:57:26 +0200

    Added property test for Maybe.Return

--- tests/CSharpx.Tests/MaybeTests.cs
+++ tests/CSharpx.Tests/MaybeTests.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
+using FsCheck;
+using FsCheck.Xunit;
+using Xunit;
 
 namespace CSharpx.Tests
 {
     public class MaybeTests
     {
+        [Fact]
+        public void Constructing_a_monadic_integer_value_allows_the_same_to_be_extracted_unchanged()
+        {
+            Prop.ForAll<int>(
+                x =>
+                {
+                    var maybeInt = Maybe.Return(x);
+                    switch (maybeInt.Tag)
+                    {
+                        case MaybeType.Just:
+                            ((Just<int>)maybeInt).Value.ShouldBeEquivalentTo(x);
+                            break;
+                        default:
+                            maybeInt.Should().BeOfType<Nothing<int>>();
+                            break;
+                    }
+                });
+        }
     }
 }
