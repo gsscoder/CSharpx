c9d67a47d791da1069d76a02cac1c069438f318c
Giacomo Stelluti Scala <gsscoder@gmail.com>
2019-12-30 06:23:35 +0100

    Removed some XML comments

--- src/CSharpx/Helpers.cs
+++ src/CSharpx/Helpers.cs
@@ -16,8 +16,6 @@ namespace CSharpx
         /// <summary>
         /// Checks if a string is composed only by alphanumeric characters.
         /// </summary>
-        /// <param name="@string">The string to check.</param>
-        /// <returns>True if it's alphanumeric, otherwise false.</returns>
         public static bool IsAlphanumeric(this string @string)
         {
             foreach (var @char in @string.ToCharArray()) {
@@ -31,8 +29,6 @@ namespace CSharpx
         /// <summary>
         /// Checks if a string is contains any kind of white spaces.
         /// </summary>
-        /// <param name="@string">The string to check.</param>
-        /// <returns>True if contains whitspaces, otherwise false</returns>
         public static bool ContainsWhiteSpace(this string @string)
         {
             foreach (var @char in @string.ToCharArray()) {
@@ -46,10 +42,6 @@ namespace CSharpx
         /// <summary>
         /// Repeats a string for a given number of times using a seperator.
         /// </summary>
-        /// <param name="@string">THe string to repeat</param>
-        /// <param name="count">Number of times to repeat the string.</param>
-        /// <param name="separator">The separator to use in the returned string.</param>
-        /// <returns>A string repeated a given number of times.</returns>
         public static string Repeat(this string @string, int count, string separator = " ")
         {
             if (count < 0) {
@@ -62,10 +54,6 @@ namespace CSharpx
         /// <summary>
         /// Applies a given function to nth-word of string.
         /// </summary>
-        /// <param name="@string">The string to which apply the function.</param>
-        /// <param name="index">The word index.</param>
-        /// <param name="modifier">The function to apply to the word.</param>
-        /// <returns></returns>
         public static string ApplyToWord(this string @string, int index, Func<string, string> modifier)
         {
             if (index < 0) {
@@ -80,9 +68,6 @@ namespace CSharpx
         /// <summary>
         /// Gets a random index of a word that optionally satisfies a function.
         /// </summary>
-        /// <param name="@string">The string of interest.</param>
-        /// <param name="validator">The function to validate the random word.</param>
-        /// <returns></returns>
         public static int RandomWordIndex(this string @string, Func<string, bool> validator = null)
         {
             Func<string, bool> _nullValidator = _ => true;
@@ -99,8 +84,6 @@ namespace CSharpx
         /// <summary>
         /// Mangles a word with a non alphanumeric character as prefix or suffix.
         /// </summary>
-        /// <param name="word">The word to mangle (must not containg spaces).</param>
-        /// <returns>The mangled word.</returns>
         public static string Mangle(this string word)
         {
             if (word.ContainsWhiteSpace()) {
@@ -119,8 +102,6 @@ namespace CSharpx
         /// <summary>
         /// Reduces a collection of strings to a collection of words.
         /// </summary>
-        /// <param name="collection"></param>
-        /// <returns>A collection of words.</returns>
         public static IEnumerable<string> ToWords(this IEnumerable<string> collection)
         {
             foreach (var element in collection) {
@@ -134,9 +115,6 @@ namespace CSharpx
         /// <summary>
         /// Inserts strings of an array between words of a string.
         /// </summary>
-        /// <param name="@string">The string to intersect.</param>
-        /// <param name="texts">The texts to intersect</param>
-        /// <returns>A string with given texts between words.</returns>
         public static string BetweenWords(this string @string, params string[] texts)
         {
             if (texts.Length == 0) {
@@ -163,9 +141,6 @@ namespace CSharpx
         /// Sanitizes a string removing non alphanumeric characters and optionally normalizing
         /// white spaces.
         /// </summary>
-        /// <param name="@string">The string to sanitize.</param>
-        /// <param name="normalizeWhiteSpace">If false will avoid normalizing white spaces.</param>
-        /// <returns></returns>
         public static string Sanitize(this string @string, bool normalizeWhiteSpace = true)
         {
             return _().Aggregate<char, string>(string.Empty, (s, c) => $"{s}{c}");
@@ -194,9 +169,6 @@ namespace CSharpx
         /// <summary>
         /// Sorts an array pure way.
         /// </summary>
-        /// <param name="array">The source array.</param>
-        /// <typeparam name="T">The type of the array.</typeparam>
-        /// <returns>The sorted array.</returns>
         public static T[] Sort<T>(this T[] array)
         {
             var copy = new T[array.Length];
@@ -208,9 +180,6 @@ namespace CSharpx
         /// <summary>
         /// Chooses a random element from an array.
         /// </summary>
-        /// <param name="array">The array of interest.</param>
-        /// <typeparam name="T">The type of the array.</typeparam>
-        /// <returns>The item randomly chosen.</returns>
         public static T Choice<T>(this T[] array)
         {
             var index = new Random().Next(array.Length - 1);
