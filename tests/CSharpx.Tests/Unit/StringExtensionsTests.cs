afed392454c2e6e267f483fc4257bab4b9c7f64a
Giacomo Stelluti Scala <gsscoder@gmail.com>
2019-12-30 08:46:09 +0100

    Used Theory/InlineData in StringExtensionsTests

--- tests/CSharpx.Tests/Unit/StringExtensionsTests.cs
+++ tests/CSharpx.Tests/Unit/StringExtensionsTests.cs
@@ -5,44 +5,50 @@ namespace CSharpx.Tests.Unit
 {
     public class StringExtensionsTests
     {
-        [Fact]
-        public void Should_detect_alphanumeric_characters()
+        [Theory]
+        [InlineData("hello", true)]
+        [InlineData("0123456789", true)]
+        [InlineData("hello01234", true)]
+        [InlineData("hello.tests", false)]
+        [InlineData("hello tests", false)]
+        public void Should_detect_alphanumeric_characters(string value, bool expected)
         {
-            "hello".IsAlphanumeric().Should().BeTrue();
-            "0123456789".IsAlphanumeric().Should().BeTrue();
-            "hello01234".IsAlphanumeric().Should().BeTrue();
-            "hello.tests".IsAlphanumeric().Should().BeFalse();
-            "hello tests".IsAlphanumeric().Should().BeFalse();
+            value.IsAlphanumeric().Should().Be(expected);
         }
 
-        [Fact]
-        public void Should_detect_whitespace_characters()
+        [Theory]
+        [InlineData("hello01234", false)]
+        [InlineData("hello.tests", false)]
+        [InlineData("hello tests", true)]
+        [InlineData("hello\ntests", true)]
+        [InlineData("hello\ttests", true)]
+        public void Should_detect_whitespace_characters(string value, bool expected)
         {
-            "hello01234".ContainsWhiteSpace().Should().BeFalse();
-            "hello.tests".ContainsWhiteSpace().Should().BeFalse();
-            "hello tests".ContainsWhiteSpace().Should().BeTrue();
-            "hello\ntests".ContainsWhiteSpace().Should().BeTrue();
-            "hello\ttests".ContainsWhiteSpace().Should().BeTrue();
+            value.ContainsWhiteSpace().Should().Be(expected);
         }
 
-        [Fact]
-        public void Should_sanitize_strings_normalizing_white_spaces()
+        [Theory]
+        [InlineData("hello tests@", "hello tests")]
+        [InlineData("hello\ttests@", "hello tests")]
+        public void Should_sanitize_strings_normalizing_white_spaces(string value, string expected)
         {
-            "hello tests@".Sanitize().Should().Be("hello tests");
-            "hello\ttests@".Sanitize().Should().Be("hello tests");
+            value.Sanitize().Should().Be(expected);
         }
 
-        [Fact]
-        public void Should_sanitize_strings_without_normalizing_white_spaces()
+        [Theory]
+        [InlineData("hello\ntests@", "hello\ntests")]
+        [InlineData("hello\ttests@", "hello\ttests")]
+        public void Should_sanitize_strings_without_normalizing_white_spaces(string value, string expected)
         {
-            "hello\ntests@".Sanitize(normalizeWhiteSpace: false).Should().Be("hello\ntests");
-            "hello\ttests@".Sanitize(normalizeWhiteSpace: false).Should().Be("hello\ttests");
+            value.Sanitize(normalizeWhiteSpace: false).Should().Be(expected);
         }
 
-        [Fact]
-        public void Should_intersperse_values()
+        [Theory]
+        [InlineData("hello this is a test", new object[] {'!', "!!", 10}, "hello ! this !! is 10 a test")]
+        public void Should_intersperse_values(string value, object[] values, string expected)
         {
-            "hello this is a test".Intersperse('!', "!!", 10).Should().Be("hello ! this !! is 10 a test");
+            // "hello this is a test".Intersperse('!', "!!", 10).Should().Be("hello ! this !! is 10 a test");
+            value.Intersperse(values).Should().Be(expected);
         }
     }
 }
