09f5b6d0935c51f32ac9039cf8b14c99b2b6ced0
Giacomo Stelluti Scala <gsscoder@gmail.com>
2020-01-01 10:40:02 +0100

    Restored Unit and Identity

---
+++ src/CSharpx/Unit.cs
@@ -0,0 +1,46 @@
+//#define CSX_UNIT_INTERNAL // Uncomment or define at build time to set Unity accessibility to internal.
+
+using System;
+
+namespace CSharpx
+{
+#if !CSX_UNIT_INTERNAL
+    public
+#endif
+    struct Unit : IEquatable<Unit>
+    {
+        private static readonly Unit @default = new Unit();
+
+        public bool Equals(Unit other)
+        {
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Unit;
+        }
+
+        public override int GetHashCode()
+        {
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return "()";
+        }
+
+        public static bool operator ==(Unit first, Unit second)
+        {
+            return true;
+        }
+
+        public static bool operator !=(Unit first, Unit second)
+        {
+            return false;
+        }
+
+        public static Unit Default { get { return @default; } }
+    }
+}
