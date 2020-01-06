c270d7319c7df0244c51f51cbb4111a349bc43be
Giacomo Stelluti Scala <gsscoder@gmail.com>
2020-01-06 18:33:07 +0100

    Added tests for Either

--- tests/CSharpx.Tests/Unit/FSharpResultExtensionsTests.cs
+++ tests/CSharpx.Tests/Unit/FSharpResultExtensionsTests.cs
@@ -1,5 +1,4 @@
 using System;
-using Xunit;
 using FluentAssertions;
 using FsCheck;
 using FsCheck.Xunit;
@@ -16,11 +15,26 @@ namespace CSharpx.Tests.Unit
         {
             int? expected = null;
             var sut = FSharpResult<int, string>.NewOk(value);
+
             sut.Match(
                 matched => expected = value,
                 _ => { throw new InvalidOperationException(); }
             );
+
             expected.Should().Be(value);
+        }
+
+        [Property(Arbitrary = new[] { typeof(ArbitraryIntegers) })]
+        public void Should_match_result_with_Either(int value)
+        {
+            var sut = FSharpResult<int, string>.NewOk(value);
+
+            var expected = sut.Either(
+                _ => true,
+                _ => false
+            );
+
+            expected.Should().BeTrue();
         }
 
         [Property(Arbitrary = new[] { typeof(ArbitraryIntegers) })]
@@ -28,11 +42,26 @@ namespace CSharpx.Tests.Unit
         {
             string error = null;
             var sut = FSharpResult<int, string>.NewError("bad result");
+
             sut.Match(
                 _ => { throw new InvalidOperationException(); },
                 message => { error = message; }
             );
+
             error.Should().Be("bad result");
+        }
+
+        [Property(Arbitrary = new[] { typeof(ArbitraryIntegers) })]
+        public void Should_match_error_with_Either(int value)
+        {
+            var sut = FSharpResult<int, string>.NewError("bad result");
+
+            var error = sut.Either(
+                _ => true,
+                _ => false
+            );
+
+            error.Should().BeFalse();
         }
     }
 }
