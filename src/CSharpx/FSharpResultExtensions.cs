3d974c7b64eadcfdfe9230bda4185b202881a6c4
Giacomo Stelluti Scala <gsscoder@gmail.com>
2020-01-07 06:27:00 +0100

    Added Trial.Lift

--- src/CSharpx/FSharpResultExtensions.cs
+++ src/CSharpx/FSharpResultExtensions.cs
@@ -68,6 +68,14 @@ namespace CSharpx.FSharp
                 }
                 return FSharpResult<T, TError>.NewError(result.ErrorValue);
             }
+
+            // Lifts a function into a result container and applies it on the given result.
+            public static FSharpResult<T, TError> Lift<TValue, T, TError>(
+                Func<TValue, T> func,
+                FSharpResult<TValue, TError> result)
+            {
+                return Apply(FSharpResult<Func<TValue, T>, TError>.NewOk(func), result);
+            }
         }
     }
 }
