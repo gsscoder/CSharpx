fcc086496f1ad1d7dd9dbf8920020d0bec9a5f9b
Giacomo Stelluti Scala <gsscoder@gmail.com>
2020-01-27 19:13:19 +0100

    Code spacing

--- src/CSharpx/FSharpResultExtensions.cs
+++ src/CSharpx/FSharpResultExtensions.cs
@@ -1,5 +1,6 @@
 //#define CSX_FSHARP_DISABLED // Uncomment or define at build time to remove all F# related types.
 //#define CSX_FSHARP_RESEXT_INTERNAL // Uncomment or define at build time to set FSharpResultExtensions accessibility to internal.
+
 #if !CSX_FSHARP_DISABLED
 using System;
 using Microsoft.FSharp.Core;
