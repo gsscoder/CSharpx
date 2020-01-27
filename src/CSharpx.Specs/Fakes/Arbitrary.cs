0b4b620bd5ad705582dc7abf3030361d862628f9
Giacomo Stelluti Scala <gsscoder@gmail.com>
2020-01-27 07:54:01 +0100

    Refactored arbitrary test types with member expr

--- src/CSharpx.Specs/Fakes/Arbitrary.cs
+++ src/CSharpx.Specs/Fakes/Arbitrary.cs
@@ -3,26 +3,18 @@ using FsCheck;
 
 static class ArbitraryIntegers
 {
-    public static Arbitrary<int> IntegerGenerator()
-    {
-        return Gen.Choose(-30, 30).ToArbitrary();
-    }
+    public static Arbitrary<int> IntegerGenerator() => Gen.Choose(-30, 30).ToArbitrary();
 }
 
 static class ArbitraryListOfIntegers
 {
-    public static Arbitrary<FSharpList<int>> IntegerListGenerator()
-    {
-        return Gen.ListOf(30, Gen.Choose(-30, 30)).ToArbitrary();
-    }
+    public static Arbitrary<FSharpList<int>> IntegerListGenerator() => Gen.ListOf(30,
+        Gen.Choose(-30, 30)).ToArbitrary();
 }
 
 static class ArbitraryStrings
 {
-    public static Arbitrary<string[]> StringGenerator()
-    {
-        return Gen.Shuffle(new [] {
+    public static Arbitrary<string[]> StringGenerator() => Gen.Shuffle(new [] {
             "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"})
             .ToArbitrary();
-    }
 }
