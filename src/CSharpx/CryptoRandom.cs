e30b3389c3f9402f04c95db3624e5cd783c7153f
Giacomo Stelluti Scala <gsscoder@gmail.com>
2020-01-28 11:05:22 +0100

    Used nameof in CryptoRandom class

--- src/CSharpx/CryptoRandom.cs
+++ src/CSharpx/CryptoRandom.cs
@@ -18,29 +18,37 @@ namespace CSharpx
         private byte[] _buffer;
         private int _bufferPosition;
 
-        /// <summary>Gets a value indicating whether this instance has random pool enabled.</summary>
+        /// <summary>
+        /// Gets a value indicating whether this instance has random pool enabled.
+        /// </summary>
         public bool IsRandomPoolEnabled
         {
             get;
             private set;
         }
 
-        /// <summary>Initializes a new instance of the <c>CryptoRandom<c/> class with. Using this
-        /// overload will enable the random buffer pool./summary>
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CryptoRandom"/> class with.
+        /// Using this overload will enable the random buffer pool.
+        /// </summary>
         public CryptoRandom() : this(true)
         {
         }
 
-        /// <summary>Initializes a new instance of the <c>CryptoRandom<c/> class. This method will
-        /// disregard whatever value is passed as seed and it's only implemented in order to be fully
-        /// backwards compatible with <c>System.Random<c/>. Using this overload will enable the random
-        /// buffer pool.</summary>
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CryptoRandom"/> class.
+        /// This method will disregard whatever value is passed as seed and it's only implemented
+        /// in order to be fully backwards compatible with <see cref="System.Random"/>.
+        /// Using this overload will enable the random buffer pool.
+        /// </summary>
         [SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "ignoredSeed",
             Justification = "Cannot remove this parameter as we implement the full API of System.Random")]
         public CryptoRandom(int seed) : this(true) { }
 
-        /// <summary>Initializes a new instance of the <c>CryptoRandom<c/> class with ptional random
-        /// buffer.</summary>
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CryptoRandom"/> class with
+        /// optional random buffer.
+        /// </summary>
         public CryptoRandom(bool enableRandomPool)
         {
             IsRandomPoolEnabled = enableRandomPool;
@@ -48,45 +56,51 @@ namespace CSharpx
 
         private void InitBuffer()
         {
-            if (IsRandomPoolEnabled) {
-                if (_buffer == null || _buffer.Length != 512) {
+            if (IsRandomPoolEnabled)
+            {
+                if (_buffer == null || _buffer.Length != 512)
                     _buffer = new byte[512];
-                }
             }
-            else {
-                if (_buffer == null || _buffer.Length != 4) {
+            else
+            {
+                if (_buffer == null || _buffer.Length != 4)
                     _buffer = new byte[4];
-                }
             }
 
             _rng.GetBytes(_buffer);
             _bufferPosition = 0;
         }
 
-        /// <summary>Returns a non-negative random integer.</summary>
+        /// <summary>
+        /// Returns a non-negative random integer.
+        /// </summary>
         public override int Next()
         {
             // Mask away the sign bit so that we always return nonnegative integers
             return (int)GetRandomUInt32() & 0x7FFFFFFF;
         }
 
-        /// <summary>Returns a non-negative random integer that is less than the specified
-        /// maximum.</summary>
+        /// <summary>
+        /// Returns a non-negative random integer that is less than the specified maximum.
+        /// </returns>
         public override int Next(int maxValue)
         {
-            if (maxValue < 0) throw new ArgumentOutOfRangeException(nameof(maxValue));
+            if (maxValue < 0)
+                throw new ArgumentOutOfRangeException(nameof(maxValue));
 
             return Next(0, maxValue);
         }
 
-        /// <summary>Returns a non-negative random integer that is within a specified range.</summary>
+        /// <summary>
+        /// Returns a non-negative random integer that is within a specified range.
+        /// </summary>
         public override int Next(int minValue, int maxValue)
         {
-            if (minValue > maxValue) throw new ArgumentOutOfRangeException(nameof(minValue));
+            if (minValue > maxValue)
+                throw new ArgumentOutOfRangeException(nameof(minValue));
 
-            if (minValue == maxValue) {
+            if (minValue == maxValue)
                 return minValue;
-            }
 
             long diff = maxValue - minValue;
 
@@ -102,26 +116,30 @@ namespace CSharpx
             }
         }
 
-        /// <summary>Returns a random floating-point number that is greater than or equal to 0.0, and
-        /// less than 1.0.</summary>
+        /// <summary>
+        /// Returns a random floating-point number that is greater than or equal to 0.0, and less than 1.0.
+        /// </summary>
         public override double NextDouble()
         {
             return GetRandomUInt32() / (1.0 + uint.MaxValue);
         }
 
-        /// <summary>Fills the elements of a specified array of bytes with random numbers.</summary>
+        /// <summary>
+        /// Fills the elements of a specified array of bytes with random numbers.
+        /// </summary>
         public override void NextBytes(byte[] buffer)
         {
-            if (buffer == null) throw new ArgumentNullException(nameof(buffer));
+            if (buffer == null)
+                throw new ArgumentNullException(nameof(buffer));
 
             lock (this)
             {
-                if (IsRandomPoolEnabled && _buffer == null) {
+                if (IsRandomPoolEnabled && _buffer == null)
                     InitBuffer();
-                }
 
                 // Can we fit the requested number of bytes in the buffer?
-                if (IsRandomPoolEnabled && _buffer.Length <= buffer.Length) {
+                if (IsRandomPoolEnabled && _buffer.Length <= buffer.Length)
+                {
                     int count = buffer.Length;
 
                     EnsureRandomBuffer(count);
@@ -130,7 +148,8 @@ namespace CSharpx
 
                     _bufferPosition += count;
                 }
-                else {
+                else
+                {
                     // Draw bytes directly from the RNGCryptoProvider
                     _rng.GetBytes(buffer);
                 }
@@ -153,16 +172,14 @@ namespace CSharpx
 
         private void EnsureRandomBuffer(int requiredBytes)
         {
-            if (_buffer == null) {
+            if (_buffer == null)
                 InitBuffer();
-            }
 
-            if (requiredBytes > _buffer.Length) throw new ArgumentOutOfRangeException(
-                nameof(requiredBytes), "Cannot be greater than random buffer.");
+            if (requiredBytes > _buffer.Length)
+                throw new ArgumentOutOfRangeException(nameof(requiredBytes), "cannot be greater than random buffer");
 
-            if ((_buffer.Length - _bufferPosition) < requiredBytes) {
+            if ((_buffer.Length - _bufferPosition) < requiredBytes)
                 InitBuffer();
-            }
         }
     }
 }
