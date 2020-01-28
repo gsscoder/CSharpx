277ffff49f4343307a2f5d7ec9287d2c7d54f6ac
Giacomo Stelluti Scala <gsscoder@gmail.com>
2020-01-28 07:07:50 +0100

    Added XML comments to Unit type

--- src/CSharpx/Unit.cs
+++ src/CSharpx/Unit.cs
@@ -4,6 +4,9 @@ using System;
 
 namespace CSharpx
 {
+    /// <summary>The <c>Unit</c> type is a type that indicates the absence of a specific value; the
+    /// <c>Unit</c> type has only a single value, which acts as a placeholder when no other value
+    /// exists or is needed.</summary>
 #if !CSX_UNIT_INTERNAL
     public
 #endif
@@ -11,41 +14,53 @@ namespace CSharpx
     {
         private static readonly Unit @default = new Unit();
 
+        /// <summary>Returns the hash code for this <c>Unit</c>.</summary>
         public override int GetHashCode()
         {
             return 0;
         }
 
+        /// <summary>Determines whether this instance and another specified  <c>Unit</c> object have
+        /// the same value.</summary>
         public bool Equals(Unit other)
         {
             return true;
         }
 
+        /// <summary>Determines whether this instance and a specified object, which must also be a
+        /// <c>Unit</c> object, have the same value.</summary>
         public override bool Equals(object obj)
         {
             return obj is Unit;
         }
 
+        /// <summary>Compares this instance with a specified object and indicates whether this instance
+        /// precedes, follows, or appears in the same position in the sort order as the specified
+        /// object.</summary>
         public int CompareTo(Unit obj)
         {
             return 0;
         }
 
+        /// <summary>Converts this instance to a string representation.</summary>
         public override string ToString()
         {
             return "()";
         }
 
+        /// <summary>Equality operator.</summary>
         public static bool operator ==(Unit first, Unit second)
         {
             return true;
         }
 
+        /// <summary>Inequality operator.</summary>
         public static bool operator !=(Unit first, Unit second)
         {
             return false;
         }
 
+        /// <summary>Singleton value.</summary>
         public static Unit Default { get { return @default; } }
     }
 }
