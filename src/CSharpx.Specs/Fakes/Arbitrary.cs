ceadec4ac26ffe2636bdd4bff9c5f40487605064
Giacomo Stelluti Scala <gsscoder@gmail.com>
2020-01-31 07:47:40 +0100

    Added strings with integers in ArbitraryListOfStrings

--- src/CSharpx.Specs/Fakes/Arbitrary.cs
+++ src/CSharpx.Specs/Fakes/Arbitrary.cs
@@ -15,7 +15,7 @@ static class ArbitraryListOfIntegers
 static class ArbitraryListOfStrings
 {
     public static Arbitrary<string[]> StringListGenerator() => Gen.Shuffle(new [] {
-            string.Empty, "one", "two", "three", null, "four", string.Empty, "five", "six", null,
-            "seven", "eight", "nine", null, "ten", string.Empty})
+            string.Empty, "one", "1", "two", "2", "three", "3", null, "four", "4", string.Empty, "five",
+            "5", "six", "6", null, "seven", "7", "eight", "8", "nine", "9", null, "ten", "10", string.Empty})
             .ToArbitrary();
 }
